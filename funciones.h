d7bc99f9be4d9011bf6ae5a5c74bb4e5a8f01157
alanalcivar-1302 <alan.alcivar@epn.edu.ec>
2025-06-24 08:03:17 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,42 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+//Tamaño de carácteres en arreglos
+//#define TAM_CEDULA 15
+#define TAM_NOMBRE 100
+#define TAM_PLACA 10
+
+//Estructura de los datos
+
+typedef struct{
+	
+	char nombrePropietario[TAM_NOMBRE];
+	int numCedula;
+	char modeloAuto[10];
+	char placa[TAM_PLACA];
+	char colorAuto[10];
+	int anioAuto;
+	
+}datosUsuario;
+
+
+//Funciones 
+
+void menu();
+
+int continuar();
+
+void opcionesUsuario(int *opcionEscoger, int rangoOpciones);float calcularValormatricula(int pagoAtiempo, int hizoRevisionVehiculo, int diasPago, float multasVehiculo, int tipoVehiculo);
+
+int validacionCaracteres(char stringValidar[]);
+
+void validacionEntradaNombre(char *stringValidar);
+	
+void validacionNumerica(int *intValidar);
+
+void validacionCedula(int *cedulaValidar);
+
+int validarPlaca(char placa[]);
+
+void pedirPlaca(char placa[]);
+#endif
