05877543fb72b8e6988c11fea3ef919b54928d70
Pol949 <erick.villanueva@epn.edu.ec>
2025-06-24 08:42:31 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "funciones.h"
-#include <time.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -22,11 +22,17 @@ int main(){
 		
 		
 		switch (opcionUsuario){
-			case 1:
-				printf("-----Registro de vehiculos-----\n");
-				
-				archivoDatos = fopen("datosVehiculos.txt","a");
-				
+		case 1:
+			printf("-----Registro de vehiculos-----\n");
+			
+			archivoDatos = fopen("datosVehiculos.txt","a");
+			
+			if (archivoDatos == NULL) {
+				printf("Error: El archivo no existe.\n");
+				registrarLog("ERROR", "main", "No se pudo abrir el archivo datosVehiculos.txt.");
+				fclose(archivoDatos);
+			}
+			else{
 				clearInputBuffer();
 				
 				
@@ -53,61 +59,63 @@ int main(){
 				
 				
 				fclose(archivoDatos);
-				break;
-			case 2:
-				printf("-----Buscar vehículo-----\n");
-				break;
-			case 3:{
+			}
+			break;
+		case 2:
+			printf("-----Buscar vehículo-----\n");
+			break;
+		case 3:{
+			
+			int nuevoPagomatricula = 1;
+			
+			while (nuevoPagomatricula) {
+				int pagoAtiempo, hizoRevisionVehiculo, diasPago, tipoVehiculo ;
+				float multasVehiculo, totalpagoMatricula ;
 				
-				int nuevoPagomatricula = 1;
+				printf("Seleccione su tipo de vehículo: \n");
+				printf(" 1. Automóvil \n 2. Motocicleta \n 3. Camión \n 4. Bus \n");
+				opcionesUsuario(&tipoVehiculo,4);
 				
-				while (nuevoPagomatricula) {
-					int pagoAtiempo, hizoRevisionVehiculo, diasPago, tipoVehiculo ;
-					float multasVehiculo, totalpagoMatricula ;
-					
-					printf("Seleccione su tipo de vehículo: \n");
-					printf(" 1. Automóvil \n 2. Motocicleta \n 3. Camión \n 4. Bus \n");
-					opcionesUsuario(&tipoVehiculo,4);
-					
-					printf("¿Realizó la revisión técnica? (1=Sí, 2=No): \n");
-					opcionesUsuario(&hizoRevisionVehiculo,2);
-					
-					printf("¿Pagó la matrícula a tiempo? (1=Sí, 2=No): \n");
-					opcionesUsuario(&pagoAtiempo,2);
-					scanf("%d", &pagoAtiempo);
-					
-					printf("¿Cuántos días han pasado desde la notificación?: \n");
-					scanf("%d", &diasPago);
-					
-					printf("Valor total de multas (sin descuentos): $\n");
-					opcionesUsuario(&multasVehiculo,1000);
-					
-					//Total a pagar de la matrícula
-					
-					totalpagoMatricula = calcularValormatricula(pagoAtiempo, hizoRevisionVehiculo, diasPago, multasVehiculo, tipoVehiculo) ;
-					
-					if (totalpagoMatricula >= 0) {
-						printf("\n--------------- COMPROBANTE DE MATRICULA ---------------\n");
-						printf("Recuerde guardar el comprobante. \n");
-						printf("Multas: $%.2f\n", multasVehiculo);
-						printf("Total a pagar: $%.2f\n", totalpagoMatricula);
-						printf("----------------------------------------------------------\n");
-					}
-					
-					// Desea revisar nuevo pago?
-					
-					printf("\n¿Desea procesar otro pago de matrícula? (1 = Sí, 0 = No): \n");
-					scanf("%d", &nuevoPagomatricula);
+				printf("¿Realizó la revisión técnica? (1=Sí, 2=No): \n");
+				opcionesUsuario(&hizoRevisionVehiculo,2);
+				
+				printf("¿Pagó la matrícula a tiempo? (1=Sí, 2=No): \n");
+				opcionesUsuario(&pagoAtiempo,2);
+				scanf("%d", &pagoAtiempo);
+				
+				printf("¿Cuántos días han pasado desde la notificación?: \n");
+				scanf("%d", &diasPago);
+				
+				printf("Valor total de multas (sin descuentos): $\n");
+				opcionesUsuario(&multasVehiculo,1000);
+				
+				//Total a pagar de la matrícula
+				
+				totalpagoMatricula = calcularValormatricula(pagoAtiempo, hizoRevisionVehiculo, diasPago, multasVehiculo, tipoVehiculo) ;
+				
+				if (totalpagoMatricula >= 0) {
+					printf("\n--------------- COMPROBANTE DE MATRICULA ---------------\n");
+					printf("Recuerde guardar el comprobante. \n");
+					printf("Multas: $%.2f\n", multasVehiculo);
+					printf("Total a pagar: $%.2f\n", totalpagoMatricula);
+					printf("----------------------------------------------------------\n");
 				}
 				
-				printf("Gracias por preferir nuestro sistema. Regrese mas tarde.\n");
-				break;
+				// Desea revisar nuevo pago?
+				
+				printf("\n¿Desea procesar otro pago de matrícula? (1 = Sí, 0 = No): \n");
+				scanf("%d", &nuevoPagomatricula);
 			}
-			case 4:
+			
+			printf("Gracias por preferir nuestro sistema. Regrese mas tarde.\n");
+			
+			break;
+		}
+		case 4:
 				printf("-----Agendar cita de revisión-----\n");
 				break;
-			case 5:
-				break;
+		case 5:
+			break;
 		default:
 			break;
 		}
