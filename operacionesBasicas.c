aa1f78e53d8fcf44fee4d9a51a061f7ce7b2c3cc
alanalcivar-1302 <alan.alcivar@epn.edu.ec>
2025-07-29 23:59:05 -0500

    Update operacionesBasicas.c

--- operacionesBasicas.c
+++ operacionesBasicas.c
@@ -12,35 +12,15 @@ void limpiarTerminal(){
 }
 	
 void menu(){
-	printf("1. Registrar vehÃ­culo \n");
-	printf("2. Buscar vehÃ­culo por placa \n");
-	printf("3. Consultar valor a pagar por matricula\n");
-	printf("4. Agendar cita de revisiÃ³n\n");
+	printf("1. Registrar veh%cculo\n", 161);         
+	printf("2. Buscar veh%cculo por placa\n", 161);   
+	printf("3. Consultar valor a pagar por matr%ccula\n", 161); 
+	printf("4. Agendar cita de revisi%cn\n", 162);   
 	printf("5. Listar vehiculos registrados\n");
 	printf("6. Salir\n");
+	
 };
 	
-	
-int continuar(){
-	int opcionUsuario;
-	printf("¿Desea realizar algo mas? (1. Si/2. No)\n");
-	scanf("%d",&opcionUsuario);
-	
-	//Validacion de la respuesta del usuario
-	while (opcionUsuario != 2 && opcionUsuario != 1){
-		printf("Opcion no valida ingrese (1. Si/2. No) \n");
-		scanf("%d",&opcionUsuario);
-	}
-	
-	if (opcionUsuario == 2){
-		printf("Gracias, vuelva pronto\n");
-		return 5; //Rompe el while del menÃº
-	}
-	else
-		return 0;
-};
-		
-		
 void clearInputBuffer(){
 	int c;
 	while ((c = getchar()) != '\n' && c != EOF);
